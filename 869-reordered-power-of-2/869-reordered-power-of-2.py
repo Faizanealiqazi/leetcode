ed9f0d74fffb5703beafb5aceed464113b9c4698
Faizan-E-Ali <59193821+Faizanealiqazi@users.noreply.github.com>
2022-08-29 17:21:26 +0500

    Time: 5050 ms (5.41%), Space: 39.6 MB (5.03%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -7,8 +7,8 @@ class Solution:
         
         num1 = [''.join(i) for i in itertools.permutations(str(n))]
         
-        for num11 in num1:
-            num = int(num11) 
+        for num in num1:
+            num = int(num) 
             if num%2 == 0 and math.ceil(math.log10(num)/math.log10(2)) == math.floor(math.log10(num)/math.log10(2)):
                 return True
         return False
