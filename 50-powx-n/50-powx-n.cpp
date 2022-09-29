6d18bfdbbbb68b8c3551245e4d0bb1a4173e26f9
Faizan-E-Ali <59193821+Faizanealiqazi@users.noreply.github.com>
2022-09-29 18:39:02 +0500

    Time: 8 ms (8.36%), Space: 6.2 MB (15.13%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0)
-        {
-            return 1.0;
-        }
-        else
-        {
-             return pow(x,n);
-        }
+      return pow(x,n);
     }
 };
