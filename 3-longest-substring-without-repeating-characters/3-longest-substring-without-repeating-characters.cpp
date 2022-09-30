2d637b2835845316134242d24bdb27c6aee45e6e
Faizan-E-Ali <59193821+Faizanealiqazi@users.noreply.github.com>
2022-09-30 18:15:00 +0500

    Time: 48 ms (32.30%), Space: 6.8 MB (96.75%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -8,35 +8,24 @@ public:
         for(int i=0;i<length;i++)
         {
             count=0;
-            string c="",a="";
             for (int j=0;j<256;j++)
             {
                 arr[j]=0;
             }
-            //arr[s[i]] = 1;
-
             int j=i;
             while (j<length)
             {
                 if(arr[s[j]]==0)
-                {
-                   
+                { 
                     arr[s[j]]=1;
                     count++;
-                    c+=s[j];
                 }
                 else if(arr[s[j]]==1)
                 {
-                    a+=s[j];
-                    cout<<"Loop break called : "<<a<<endl;;
-                    
                     break;
                 }
                 j++;
-
             }
-             cout<<"\nString: "<<c<<" nnnn   "<<a<<endl;
-
             if (count>max)
             {
                 max = count;
